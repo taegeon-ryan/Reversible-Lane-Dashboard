f4f5a5d70e329a995bcba247e3e0170f7dfe273c
내맘이네전부를기억해 <ahgns1515@naver.com>
2019-08-30 21:52:22 +0900

    v 0.0.1
    download program and mysql test

---
+++ src/config/dbconfig.js
@@ -0,0 +1,8 @@
+var dbconfig = {
+    host: 'localhost',
+    user: 'swhackathon',
+    password: 'arduino',
+    database: 'hackathon'
+};
+
+module.exports = dbconfig;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,12 @@
 const { Router } = require('express');
+var mysql = require('mysql');
+const session = require('express-session');
+var crypto = require('crypto');
+var MySQLStore = require('express-mysql-session')(session);
+var dbconfig = require('../config/dbconfig');
+var db = mysql.createConnection(dbconfig);
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 const router = Router();
 
@@ -7,6 +15,34 @@ router.get('/', (req, res) => {
   res.render('index', {
     title: 'Express'
   });
+});
+
+router.get('/test', (req, res)=> {
+  var selectsql = 'SELECT * FROM `signal_data` ORDER BY signal_idx DESC';
+  db.query(selectsql, function (error, results) {
+    if (error) {
+      status = "err";
+      console.log("err");
+    }
+    else  {
+      if(!results[0]){
+        console.log("no");
+      }else{
+        console.log(results);
+      }
+    }
+    res.render('index',{
+      title: 'Express'
+    });
+  });
+});
+
+router.post('/upload_live',(req, res)=>{
+  
+});
+
+router.post('/upload_signal', (req, res) =>{
+
 });
 
 module.exports = router;
