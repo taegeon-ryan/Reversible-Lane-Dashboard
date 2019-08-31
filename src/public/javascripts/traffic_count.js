2c03d7964d5ed35460454929fbce5afdf26390e5
taegeon-ryan <taegeonryan@gmail.com>
2019-08-31 12:49:19 +0900

    도로 현황 LED X자 구현

---
+++ src/public/javascripts/traffic_count.js
@@ -0,0 +1,16 @@
+function changeColor() {
+  var el = document.getElementById('asc-sum');
+  el.style.backgroundColor = '#fab005';
+  var el = document.getElementById('asc-1');
+  el.style.backgroundColor = '#e82828';
+  var el = document.getElementById('asc-2');
+  el.style.backgroundColor = '#e82828';
+  var el = document.getElementById('asc-3');
+  el.style.backgroundColor = '#fab005';
+  var el = document.getElementById('asc-6');
+  el.style.backgroundColor = '#fab005';
+  var el = document.getElementById('desc-6');
+  el.style.backgroundColor = '#fab005';
+  var el = document.getElementById('desc-8');
+  el.style.backgroundColor = '#fab005';
+}
